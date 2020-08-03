3cd3ed00ed7d1f193bd7ef0c9d26936a3f00503c
Fraciscv <Francisco.Andaur@icloud.com>
2020-08-03 18:31:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    let thankYou = []
+    for (let i = 0; i < name.length; i++) {
+        thankYou.push( `Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYou
+}
+
+function countDown( number) {
+    while (number > 0 ) {
+        console.log(number);
+        number--;
+    }
+    console.log(number);
+}
